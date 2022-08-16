765ac7986a467365155c1f6bacf59b4cf16f6b0b
TaeYOng90 <rltdlavmem32@gmail.com>
2022-08-16 17:24:15 +0900

    testcommit 2

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import '../css/image.scss';
-const Image = () => {
-  return (
-    <div>
-      <span>이미지</span>
-    </div>
-  );
-};
+import React, { useState, useCallback } from 'react';
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from 'react-images';
+import { photos } from './photo';
+// import './Style.css'
 
-export default Image;
+
+function App() {
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+
+  const openPhoto = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+  const closeImage = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+
+    <div>
+      <button>하이요</button>
+      <h1 className="myheader">제목창입니다</h1>
+      <Gallery photos={photos} onClick={openPhoto}></Gallery>
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeImage}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div >
+  )
+}
+export default App
