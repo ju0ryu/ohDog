beb8a966b80a2d33c2951ecb0afecb7989e001e3
leewajoon <leewajoon@naver.com>
2022-08-18 14:29:09 +0900

    myfeedend

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,13 @@
 import React, { useState, useCallback } from 'react';
-import Gallery from "react-photo-gallery";
+import Gallery from 'react-photo-gallery';
 import Carousel, { Modal, ModalGateway } from 'react-images';
 import { photos } from './photo';
-// import './Style.css'
-
+import { Link } from 'react-router-dom';
+import '../css/image.scss';
 
 function App() {
-
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
 
   const openPhoto = useCallback((event, { photo, index }) => {
     setCurrentImage(index);
@@ -21,26 +19,43 @@ function App() {
   };
 
   return (
-
     <div>
-      <button>하이요</button>
-      <h1 className="myheader">제목창입니다</h1>
+      <nav id="menu">
+        <h1>
+          <ul>
+            <Link to="/myFeed">
+              <li>
+                <a class="feedlink" href="#">
+                  MYFEED
+                </a>
+              </li>
+            </Link>
+            <Link to="/image">
+              <li>
+                <a class="photolink" href="#">
+                  MYPHOTO
+                </a>
+              </li>
+            </Link>
+          </ul>
+        </h1>
+      </nav>
       <Gallery photos={photos} onClick={openPhoto}></Gallery>
       <ModalGateway>
         {viewerIsOpen ? (
           <Modal onClose={closeImage}>
             <Carousel
               currentIndex={currentImage}
-              views={photos.map(x => ({
+              views={photos.map((x) => ({
                 ...x,
                 srcset: x.srcSet,
-                caption: x.title
+                caption: x.title,
               }))}
             />
           </Modal>
         ) : null}
       </ModalGateway>
-    </div >
-  )
+    </div>
+  );
 }
-export default App
+export default App;
