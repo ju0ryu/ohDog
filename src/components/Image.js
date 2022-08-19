a22cd3152a718d34c90fbb4e07e87641c382ccbe
TaeYOng90 <rltdlavmem32@gmail.com>
2022-08-19 18:33:50 +0900

    TaeYong002 image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback } from 'react';
+import React, { useRef, useState, useCallback, useEffect } from 'react';
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from 'react-images';
 import { photos } from './photo';
@@ -14,18 +14,64 @@ const Image = ({ handlelist }) => {
   const userid = 'userid 01'
   const imgurl = useRef();
   // const imgdataRef = useRef();
-  const secret = 'Y'
+  var secret = 'Y'
+
+
+
+  // 시크릿 변경 공개 비공개
+  const onChange = (e) => {
+    // console.log(e.target.value);
+    secret = e.target.value;
+    console.log(secret);
+  };
 
 
 
   const [image_name, setImage_name] = useState("");
-  // 이미지 상태 추가??
-  const [imgBase64, setImgBase64] = useState("");
+  // 이미지 상태 추가??\
+
+
+  // const [imgBase64, setImgBase64] = useState("");
   // 이미지 미리보기?
+
+
+
 
   function onImage(e) {
     setImage_name(e.target.files[0]);
   }
+
+
+
+  // 포토 갱신???
+  // const [photos, setPhotos] = useState(
+  //   { Photos: [], }
+  // );
+
+
+
+
+  // useEffect(() => {
+  //   axios
+  //     .post('http://localhost:8008/ilist', { userid })
+  //     .then((res) => {
+  //       console.log('res ==>', res);
+  //       const { data } = res;
+  //       console.log('data ==>', data);
+  //       setPhotos({
+  //         Photos: data,
+  //       });
+  //       // 여기도 navigate 써야하나?
+  //     })
+  //     .catch((e) => {
+  //       console.error(e);
+  //     });
+  // }, []);
+
+
+
+
+
 
   const handleInsert = (e) => {
     console.log("handleInsert =>", imgurl.current.value);
@@ -54,13 +100,17 @@ const Image = ({ handlelist }) => {
       )
       .then((res) => {
         console.log("handleInsert =>", res);
-
+        // handlelist();
         imgurl.current.value = "";
       })
       .catch((e) => {
         console.error(e);
       });
   };
+
+
+
+
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
@@ -118,6 +168,26 @@ const Image = ({ handlelist }) => {
               onChange={onImage}
             />
           </td>
+          <td>
+            <input
+              type="radio"
+              name="cs_open"
+              id="cs_open"
+              value="Y"
+              class="radio"
+              onChange={onChange}
+            ></input>
+            <span>공개</span>&nbsp;&nbsp;
+            <input
+              type="radio"
+              name="cs_open"
+              id="cs_open"
+              value="N"
+              class="radio"
+              onChange={onChange}
+            />
+            <span>비공개</span>&nbsp;
+          </td>
           {/* 이미지 추가된 내용 */}
           {/* <div>
             {image_name && <img src={image_name} alt={onImage} />}
@@ -143,9 +213,12 @@ const Image = ({ handlelist }) => {
       <h1 className="myheader">어떻게 연결 시키지????</h1>
 
 
-
+      {/* 갤러리 포토 밑에 뿌려주는거 */}
       <Gallery photos={photos} onClick={openPhoto}></Gallery>
+
+      {/* 창뛰어주는듯? 모델게이트웨이???? */}
       <ModalGateway>
+
         {viewerIsOpen ? (
           <Modal onClose={closeImage}>
             <Carousel
@@ -153,11 +226,13 @@ const Image = ({ handlelist }) => {
               views={photos.map(x => ({
                 ...x,
                 srcset: x.srcSet,
-                caption: x.title
+                caption: x.title,
               }))}
             />
           </Modal>
         ) : null}
+
+
       </ModalGateway>
     </div >
   );
